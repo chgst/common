ff6cefa972e25beff7c7eab3379bdeb998ee494a
Janusz Slota <janusz.slota@nixilla.com>
2018-08-08 11:12:53 +0100

    adds command construtor

--- src/Changeset/Command/Command.php
+++ src/Changeset/Command/Command.php
@@ -8,4 +8,12 @@ use Changeset\Common\OnAggregateTrait;
 class Command implements CommandInterface
 {
     use CommandTrait, OnAggregateTrait, HasPayloadTrait;
+
+    public function __construct(string $name, string $aggregateType, string $aggregateId, $payload)
+    {
+        $this->setName($name);
+        $this->setAggregateType($aggregateType);
+        $this->setAggregateId($aggregateId);
+        $this->setPayload($payload);
+    }
 }
