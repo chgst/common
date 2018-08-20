40a71c71ffa85c25b9bf6f205f6d55c5c24bb502
Janusz Slota <janusz.slota@nixilla.com>
2018-08-20 17:22:26 +0100

    adds setter for repository implementation

--- src/Changeset/Command/HandlerInterface.php
+++ src/Changeset/Command/HandlerInterface.php
@@ -2,8 +2,11 @@
 
 namespace Changeset\Command;
 
+use Changeset\Event\RepositoryInterface;
+
 interface HandlerInterface
 {
+    public function setRepository(RepositoryInterface $repository): self;
     public function supports(CommandInterface $command): bool;
     public function process(CommandInterface $command);
 }
