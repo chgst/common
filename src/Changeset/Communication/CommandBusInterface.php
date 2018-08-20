a72fa18493cb641163b4e0eb102ec7aa3204b80b
Janusz Slota <janusz.slota@nixilla.com>
2018-08-20 16:28:16 +0100

    adds addHandler to command bus interface

--- src/Changeset/Communication/CommandBusInterface.php
+++ src/Changeset/Communication/CommandBusInterface.php
@@ -3,8 +3,10 @@
 namespace Changeset\Communication;
 
 use Changeset\Command\CommandInterface;
+use Changeset\Command\HandlerInterface;
 
 interface CommandBusInterface
 {
+    public function addHandler(HandlerInterface $handler);
     public function dispatch(CommandInterface $command);
 }
