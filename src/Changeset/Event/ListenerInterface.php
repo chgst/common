e727135f7fa972a37b61cb26ae276e28c011da5f
Janusz Slota <janusz.slota@nixilla.com>
2018-08-22 15:21:41 +0100

    changes var name

--- src/Changeset/Event/ListenerInterface.php
+++ src/Changeset/Event/ListenerInterface.php
@@ -4,6 +4,6 @@ namespace Changeset\Event;
 
 interface ListenerInterface
 {
-    public function supports(EventInterface $command): bool;
+    public function supports(EventInterface $event): bool;
     public function process(EventInterface $event);
 }
--- src/Changeset/Event/ProjectorInterface.php
+++ src/Changeset/Event/ProjectorInterface.php
@@ -4,6 +4,6 @@ namespace Changeset\Event;
 
 interface ProjectorInterface
 {
-    public function supports(EventInterface $command): bool;
+    public function supports(EventInterface $event): bool;
     public function process(EventInterface $event);
 }
