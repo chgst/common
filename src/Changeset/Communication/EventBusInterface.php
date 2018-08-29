f6fb51cfd9176e7ca555bdb7a84ae3c6b5627453
Janusz Slota <janusz.slota@nixilla.com>
2018-08-29 16:24:25 +0100

    adds additional EventBus methods

--- src/Changeset/Communication/EventBusInterface.php
+++ src/Changeset/Communication/EventBusInterface.php
@@ -3,8 +3,15 @@
 namespace Changeset\Communication;
 
 use Changeset\Event\EventInterface;
+use Changeset\Event\ListenerInterface;
+use Changeset\Event\ProjectorInterface;
 
 interface EventBusInterface
 {
+    public function enableListeners();
+    public function disableListeners();
+    public function listenersEnabled(): bool;
+    public function addListener(ListenerInterface $listener);
+    public function addProjector(ProjectorInterface $projector);
     public function dispatch(EventInterface $event);
 }
