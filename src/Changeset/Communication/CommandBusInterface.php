5717781716102d81c54f8a952e8713b6e081107e
Janusz Slota <janusz.slota@nixilla.com>
2018-08-30 10:59:49 +0100

    changes adder to setter for generic handler

--- src/Changeset/Communication/CommandBusInterface.php
+++ src/Changeset/Communication/CommandBusInterface.php
@@ -8,6 +8,6 @@ use Changeset\Command\HandlerInterface;
 interface CommandBusInterface
 {
     public function setEventBus(EventBusInterface $eventBus);
-    public function addHandler(HandlerInterface $handler);
+    public function setHandler(HandlerInterface $handler);
     public function dispatch(CommandInterface $command);
 }
