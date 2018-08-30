43d7ffa78f725444ab18ca80a5989e28808a492f
Janusz Slota <janusz.slota@nixilla.com>
2018-08-30 10:57:22 +0100

    removed supports method so we can have generic handler

--- src/Changeset/Command/HandlerInterface.php
+++ src/Changeset/Command/HandlerInterface.php
@@ -8,6 +8,5 @@ use Changeset\Event\RepositoryInterface;
 interface HandlerInterface
 {
     public function setRepository(RepositoryInterface $repository);
-    public function supports(CommandInterface $command): bool;
     public function process(CommandInterface $command): EventInterface;
 }
